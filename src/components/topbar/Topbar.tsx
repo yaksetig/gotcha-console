70da0876ff2a6a4646760edfd4d058b7a55f9385
Mario Yaksetig <myaksetig@gmail.com>
2025-07-29 07:09:01 -0500

    Remove application dropdown from top bar

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -1,20 +1,13 @@
-import { getAccessToken, getSession } from "@auth0/nextjs-auth0";
-import { ApplicationSelector } from "./ApplicationSelector";
-import { getApplications } from "@/lib/server/console";
+import { getSession } from "@auth0/nextjs-auth0";
 import MenuButton from "./MenuButton";
 
 export default async function Topbar() {
-  const [tokenRes, session] = await Promise.all([
-    getAccessToken(),
-    getSession(),
-  ]);
-  const appsList = await getApplications(tokenRes.accessToken!!);
+  const session = await getSession();
 
   return (
     <header className="px-4 h-16 flex items-center justify-between bg-background flex-shrink-0">
       <div className="flex items-center text-gray-300">
         <MenuButton />
-        <ApplicationSelector appsList={appsList} />
       </div>
       <div className="flex items-center space-x-4">
         <span className="text-gray-300 hover:text-gray-100">
